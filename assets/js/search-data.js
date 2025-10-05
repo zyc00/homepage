1937b1217211f53483421f5fe4dabfb7e96e0771
zyc00 <61403116+zyc00@users.noreply.github.com>
2025-10-05 05:28:53 +0000

    Deploying to gh-pages from @ zyc00/homepage@02d35ecadf8d0887d8d1cdd1832847efe3cf3257 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "Publications",
-          description: "For full publication list, please refer to my Google Scholar page.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
