2262c381d9c1e99c06e2b46f80fb4aaae912accb
zyc00 <61403116+zyc00@users.noreply.github.com>
2025-10-05 05:07:47 +0000

    Deploying to gh-pages from @ zyc00/homepage@881930b67f58a38dfaaaa6dfffd25fed14b7af58 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,70 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "Home",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/al-folio/";
+    },
+  },{id: "nav-publications",
+          title: "Publications",
+          description: "For full publication list, please refer to my Google Scholar page.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/publications/";
+          },
+        },{id: "nav-repositories",
+          title: "Repositories",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/repositories/";
+          },
+        },{id: "nav-cv",
+          title: "CV",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/al-folio/assets/pdf/yuchen_cv.pdf";
+          },
+        },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/al-folio/books/the_godfather/";
+            },},{id: "news-point-sam-is-accepted-by-iclr-2025",
+          title: 'Point-SAM is accepted by ICLR 2025.',
+          description: "",
+          section: "News",},{id: "news-partslip-is-accepted-by-iccv-2025-workshop-wild3d",
+          title: 'PartSLIP++ is accepted by ICCV 2025 Workshop Wild3D.',
+          description: "",
+          section: "News",},{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
